14ba7128637e9f2451d71396a05122ba119b342e
somos-protagonistas <protagonistastotal@gmail.com>
2026-09-01 12:31:38 -0500

    Pastoral+ V2.5.2 - Versión piloto inicial
    Versión piloto funcional con Sermón 360, herramientas pastorales, módulo Aprender integrado, acceso mediante login y opción de cerrar sesión.

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='pastoral-plus-v2.5.2';
+const ASSETS=['./','./index.html','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>clients.claim()))});
+self.addEventListener('fetch',e=>{if(new URL(e.request.url).origin!==location.origin)return;if(e.request.mode==='navigate')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));else e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
